c4c282d65d46f2e965d0896e98a4374a5e7e7825
tuong <110910077+tuongkhtn@users.noreply.github.com>
2024-10-03 14:02:34 +0700

    UI user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -4,6 +4,11 @@ const handleHelloWorld = (req, res) => {
     return res.render("home.ejs");
 }   
 
+const handleUser = (req, res) => {
+    return res.render("user.ejs")
+}
+
 module.exports = {
-    handleHelloWorld
+    handleHelloWorld,
+    handleUser,
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { handleHelloWorld } from "../controller/homeController";
+import { handleHelloWorld, handleUser } from "../controller/homeController";
 
 const router = express.Router();
 
 const initWebRoutes = (app) => {
     router.get("/", handleHelloWorld);
+    router.get("/user", handleUser);
 
     return app.use("/", router);
 }
