27c0c628327e9ae11ed6e96ae06a252d019aa958
tuong <110910077+tuongkhtn@users.noreply.github.com>
2024-10-09 18:29:19 +0700

    Add relationship between table

--- src/models/user.js
+++ src/models/user.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      User.belongsTo(models.Group);
+      User.belongsToMany(models.Project, { through: 'Project_User' });
     }
   }
   User.init({
