4c0f6caa96ded7dc9afb0079a3193dc454770092
tuong <110910077+tuongkhtn@users.noreply.github.com>
2024-10-11 12:08:06 +0700

    Responsive login

--- jwtFrontend/src/components/Login/login.js
+++ jwtFrontend/src/components/Login/login.js
@@ -3,26 +3,31 @@ import "./login.scss"
 
 const Login = (props) => {
     return (
-        <div className="login-container mt-3">
+        <div className="login-container pt-5">
             <div className="container">
-                <div className="row">
-                    <div className="content-left col-7">
-                        <div>
+                <div className="row px-3">
+                    <div className="content-left col-7 d-none col-sm-7 d-sm-block">
+                        <div className="brand">
                             facebook
                         </div>
-                        <div>
+                        <div className="detail">
                             Facebook helps you connect and share with the people in your life.
                         </div>
                     </div>
 
-                    <div className="content-right col-5 d-flex flex-column gap-3 py-3 gray">
+                    <div className="content-right col-12 col-sm-5 d-flex flex-column gap-3 py-3">
+                        <div className="brand d-sm-none text-center">
+                            facebook
+                        </div>
                         <input type="text" className="form-control" placeholder="Email address or phone number"/>
                         <input type="password" className="form-control" placeholder="Password"/>
-                        <button className="btn btn-primary">Log in</button>
-                        <span className="text-center">Forgotten password?</span>
+                        <button className="btn btn-primary btn-loggin">Log in</button>
+                        <span className="text-center">
+                            <a href="#" className="forgot-password">Forgotten password?</a>
+                        </span>
                         <hr/>
                         <div className="text-center">
-                            <button className="btn btn-success">Create new account</button>
+                            <button className="btn btn-success btn-create-account">Create new account</button>
                         </div>
                     </div>
                 </div>
