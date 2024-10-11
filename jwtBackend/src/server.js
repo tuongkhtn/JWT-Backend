50e1ea79298882d59c8d60450c96304b3fd389d2
tuong <110910077+tuongkhtn@users.noreply.github.com>
2024-10-11 15:52:07 +0700

    Add middleware to allow react url

--- jwtBackend/src/server.js
+++ jwtBackend/src/server.js
@@ -7,6 +7,16 @@ require("dotenv").config()
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env.REACT_URL); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 
 // config view engine
 configViewEngine(app);
